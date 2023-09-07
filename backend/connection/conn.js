93d0d4eba7a10c5616831d71c58850c1808c27ca
Khubaib Ahmed <soulmind.os@gmail.com>
2023-09-06 22:10:02 -0400

    Tried the connection, not working so far. But it's a start

--- backend/connection/conn.js
+++ backend/connection/conn.js
@@ -1,1 +1,17 @@
-const { MongoClient } = require("mongodb")
+const { MongoClient } = require("mongodb");
+const uri = process.env.ATLAS_URI;
+
+class Connection {
+    constructor() {
+        this.client = new MongoClient(uri);
+    }
+
+    async init() {
+        await this.client.connect();
+        console.log('Connected');
+
+        this.notes = this.client.db("User's Notes");
+    }
+}
+
+module.exports = new Connection();
